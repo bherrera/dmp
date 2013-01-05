9ad1082e6c4209731043205ebdd46c1d0915e00c
Bruno Herrera <bmh@certi.org.br>
2013-01-05 00:38:35 -0200

    First integration with Chrome revision 175207

---
+++ cmake/Toolchain-clang.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR i686)
+
+set(CMAKE_C_COMPILER ${PROJECT_SOURCE_DIR}/prebuilt/llvm-allocated-type/Linux_x64/bin/clang)
+set(CMAKE_CXX_COMPILER ${PROJECT_SOURCE_DIR}/prebuilt/llvm-allocated-type/Linux_x64/bin/clang++)
+
